b47da6db666c148da3d9cb7cd140b7a46bc645a2
FedorKandiranda <FedorKandiranda@gmail.com>
2022-10-18 18:10:05 +0300

    add test for NameAndCountryComponent

--- src/app/name-and-country-form/name-and-country-form.component.spec.ts
+++ src/app/name-and-country-form/name-and-country-form.component.spec.ts
@@ -22,4 +22,27 @@ describe('NameAndCountryFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show two inputs (Name, Surname) and a dropdown list with three options (Belarus, Poland, Ukraine)', () => {
+    const expectedInputsControlNames = ['Name', 'Surname'];
+    const expectedDropdownControlName = 'Country';
+    const expectedDropdownLabel = 'Select country';
+    const expectedDropdownOptions = ['Belarus', 'Poland', 'Ukraine'];
+    const el: HTMLElement = fixture.debugElement.nativeElement;
+    const formInputs = el.querySelectorAll('input');
+    const formDropdown = el.querySelector('mat-select');
+    const formDropdownLabel = el.querySelector('.select_country_dropdown mat-label');
+    const formDropdownOptions = el.querySelectorAll('mat-select mat-option');
+    expect(formInputs[0]).toBeDefined();
+    expect(formInputs[1]).toBeDefined();
+    expect(formDropdown).toBeDefined();
+    expect(formInputs[0].getAttribute('formControlName')).toBe(expectedInputsControlNames[0]);
+    expect(formInputs[1].getAttribute('formControlName')).toBe(expectedInputsControlNames[1]);
+    expect(formDropdown?.getAttribute('formControlName')).toBe(expectedDropdownControlName);
+    expect(formDropdownLabel?.outerHTML).toContain(expectedDropdownLabel);
+    expect(formDropdownOptions[0].getAttribute('value')).toBe(expectedDropdownOptions[0]);
+    expect(formDropdownOptions[1].getAttribute('value')).toBe(expectedDropdownOptions[1]);
+    expect(formDropdownOptions[2].getAttribute('value')).toBe(expectedDropdownOptions[2]);
+    expect(formDropdownOptions[3]).toBeUndefined();
+  });
 });
