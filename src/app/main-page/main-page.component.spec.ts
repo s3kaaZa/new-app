0e7d5805e6ec372829d8f713ef4168a0788c50a0
FedorKandiranda <FedorKandiranda@gmail.com>
2022-10-18 18:09:07 +0300

    add test for MainPageComponent

--- src/app/main-page/main-page.component.spec.ts
+++ src/app/main-page/main-page.component.spec.ts
@@ -18,6 +18,19 @@ describe('MainPageComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
+  });
+
+  it('#greeting should show a greeting', () => {
+    const greetingEl: HTMLElement = fixture.debugElement.nativeElement;
+    const greeting = greetingEl.querySelector('#greeting');
+    expect(greeting?.textContent).toContain('Hi there!');
+  });
+
+  it('should show a link with routerLink equal to /state-page', () => {
+    const el: HTMLElement = fixture.debugElement.nativeElement;
+    const link = el.querySelector('a');
+    expect(link).toBeDefined();
+    expect(link?.getAttribute('routerLink')).toEqual('/state-page');
   });
 });
