7c4f6031932db196625cad39c9170eff5476170d
FedorKandiranda <FedorKandiranda@gmail.com>
2022-10-26 11:10:43 +0300

    add styles

--- src/app/modules/set-and-show-user/preview/preview.component.ts
+++ src/app/modules/set-and-show-user/preview/preview.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-preview-page',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
 })
 export class PreviewComponent {
 }
